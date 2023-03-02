93ab86968397ad8dbca895f1ae21054691372070
ali26kh26 <fcb.akh.26@gmail.com>
2023-03-02 23:14:41 +0330

    fix: make overview screen title dynamic

--- screens/meals-overview-screen.js
+++ screens/meals-overview-screen.js
@@ -1,12 +1,23 @@
+import { useLayoutEffect } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import MealItem from "../components/meal-item";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 
-const MealsOverviewScreen = ({ route }) => {
+const MealsOverviewScreen = ({ route, navigation }) => {
   const { categoryId } = route.params;
   const displayedMeals = MEALS.filter(
     (meal) => meal.categoryIds.indexOf(categoryId) >= 0
   );
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === categoryId
+    ).title;
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [categoryId, navigation]);
+
   return (
     <View style={styles.container}>
       <FlatList
